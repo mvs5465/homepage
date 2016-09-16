9473d66da7d0020a1babc691b450138b67f6df16
Matthew Schwartz <mvs5465@gmail.com>
2016-09-15 22:22:45 -0400

    Added tab functionality and homepage with navigation buttons

--- public/controller.js
+++ public/controller.js
@@ -1,10 +1,39 @@
-var app = angular.module('mainApp', []);
-app.controller('mainController', function ($scope) {
-    $scope.greeting = "Hello World How's it going";
-    $scope.title = title;
-});
-var title = {
-    heading: "Matt's Recipes"
-    , description: "A place for me to share meals I have made or want to make."
-    , body: "Here is where we will place our recipes."
-};
+(function () {
+    'use strict';
+    var app = angular.module('mainApp', []);
+    app.controller('mainController', function ($scope) {
+        $scope.greeting = "Hello World How's it going";
+        $scope.title = title;
+    });
+    app.controller('tabController', function ($scope) {
+        $scope.tab = 0;
+        $scope.setTabIndex = function (tabNum) {
+            $scope.tab = tabNum;
+        }
+        $scope.tabActive = function (num) {
+            return ($scope.tab === num);
+        }
+    });
+    app.directive('tabHome', function () {
+        return {
+            restrict: 'E'
+            , templateUrl: 'tab-home.html'
+        };
+    });
+    app.directive('tabParks', function () {
+        return {
+            restrict: 'E'
+            , templateUrl: 'tab-parks.html'
+        };
+    });
+    app.directive('tabRecipes', function () {
+        return {
+            restrict: 'E'
+            , templateUrl: 'tab-recipes.html'
+        };
+    });
+    var title = {
+        heading: "The Brewhause"
+        , description: " - Home -"
+    };
+})();
